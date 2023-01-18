1b57d96b77e38ba6a93a55071079b5445a1e2378
Jack Morgan <w0782292@myscc.ca>
2023-01-18 18:12:39 -0500

    Created array and added methods to class

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,19 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+    private _pokemons: IContent[];
+    constructor(pokemon:IContent){
+        this._pokemons = [];
+    }
+    get pokemons() : IContent[]{
+        return this._pokemons;
+    }
+
+    addContent(pokemonToAdd: IContent){
+        this._pokemons.push(pokemonToAdd)
+    }
+
+    getLength(){
+        return this._pokemons.length
+    }
 }
