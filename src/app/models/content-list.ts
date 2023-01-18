d1b03d0756aa6a7d8f38d94f6790c51066253cb8
Jack Morgan <w0782292@myscc.ca>
2023-01-18 18:46:03 -0500

    created toString method

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -10,10 +10,20 @@ export class ContentList {
     }
 
     addContent(pokemonToAdd: IContent){
-        this._pokemons.push(pokemonToAdd)
+        this._pokemons.push(pokemonToAdd);
     }
 
     getLength(){
-        return this._pokemons.length
+        return this._pokemons.length;
+    }
+
+    toString(number: number){
+        let indexItem = this._pokemons[number];
+        let indexItemTags = indexItem.tags!;
+        let output = "<h1>Title: ".concat(indexItem.name, "</h1><br /><p>Description: ", indexItem.description!, "</p><br /><p>Author: ", indexItem.author, "</p><br /><img src='", indexItem.imgSrc!, "'/><br /><p>Type: ", indexItem.type, "</p><br /><h2>Tags:</h2><br />");
+        for(let i = 0; i < indexItemTags.length; i++){
+            output.concat("<ul>", i, "</ul>")
+        }
+        
     }
 }
