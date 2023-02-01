d7d3dd386a5a51f167a28c00442b3403b058f383
Jack Morgan <w0782292@myscc.ca>
2023-02-01 18:42:17 -0500

    Added images

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -14,7 +14,7 @@ export class ContentListComponent {
     this.listOfPokemon.push({
       id: 1,
       name: 'Empoleon',
-      imgSrc: 'Csrc\assets\crobat.png',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/395.png',
       description: 'It swims as fast as a jet boat. The edges of its wings are sharp and can slice apart drifting ice.',
       author: 'Jack Morgan',
       type: 'Water, Steel',
@@ -24,7 +24,7 @@ export class ContentListComponent {
     {
       id: 2,
       name: 'Gengar',
-      imgSrc: '094.png',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png',
       description: "To steal the life of its targets, it slips into the prey's shadow and silently waits for an opportunity.",
       author: 'Jack Morgan',
       type: 'Ghost, Poison',
@@ -34,7 +34,7 @@ export class ContentListComponent {
     {
       id: 3,
       name: 'Crobat',
-      imgSrc: '169.png',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/169.png',
       description: "Both of its legs have turned into wings. Without a sound, Crobat flies swiftly toward its prey and sinks its fangs into the ape of it's target.",
       author: 'Jack Morgan',
       type: 'Poison, Flying',
@@ -44,7 +44,7 @@ export class ContentListComponent {
     {
       id: 4,
       name: 'Ampharos',
-      imgSrc: '181.png',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/181.png',
       description: "The bright light on its tail can be seen far away. It has been treasured since ancient times as a beacon.",
       author: 'Jack Morgan',
       type: 'Electric',
