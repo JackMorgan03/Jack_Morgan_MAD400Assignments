ecefafa24a002924249ec4597f83153cd160eda9
Jack Morgan <w0782292@myscc.ca>
2023-02-05 20:25:03 -0500

    Completed step 9

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -13,4 +13,37 @@ export class PokemonService {
   getContent(): Observable<IContent[]>{
     return of(POKEMON)
   }
+
+
+  // getContentItem(index: number): Observable<IContent>{
+  //   POKEMON.forEach(pokemon => {
+  //     if(pokemon.id == index){
+  //       return of(pokemon)
+  //     }
+  //   });
+  // }
+
+  createItem(pokemon: IContent): Observable<IContent[]>{
+    POKEMON.push(pokemon);
+    return of(POKEMON);
+  }
+
+  updateItem(pokemon: IContent): Observable<IContent[]>{
+      POKEMON.forEach(poke=>{
+        if (pokemon.id = poke.id){
+          pokemon = poke
+        }
+      })
+      return of(POKEMON);
+  }
+
+  deleteItem(id:number){
+    POKEMON.forEach(removePokemon => {
+      if(removePokemon.id = id){
+        POKEMON.splice(removePokemon.id, 1)
+        return removePokemon;
+      }
+      return console.log("Not this one");
+    });
+  }
 }
