deb3629ed8506832932e03def7b24fce06698b6a
Jack Morgan <w0782292@myscc.ca>
2023-04-17 22:04:38 -0400

    Completed steps 1-5

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -6,7 +6,7 @@ export const POKEMON: IContent[] = [
       imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/395.png',
       description: 'It swims as fast as a jet boat. The edges of its wings are sharp and can slice apart drifting ice.',
       author: 'Jack Morgan',
-      type: 'Water, Steel',
+      type: ['Water, Steel'],
       tags: ["Water", "Steel", "#0395"]
     },
 
@@ -16,7 +16,7 @@ export const POKEMON: IContent[] = [
       imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png',
       description: "To steal the life of its targets, it slips into the prey's shadow and silently waits for an opportunity.",
       author: 'Jack Morgan',
-      type: 'Ghost, Poison',
+      type: ['Ghost, Poison'],
       tags: ["Ghost", "Poison", "#0094"]
     },
 
@@ -26,7 +26,7 @@ export const POKEMON: IContent[] = [
       imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/169.png',
       description: "Both of its legs have turned into wings. Without a sound, Crobat flies swiftly toward its prey and sinks its fangs into the ape of it's target.",
       author: 'Jack Morgan',
-      type: 'Poison, Flying',
+      type: ['Poison, Flying'],
       tags: ["Poison", "Flying", "#0169"]
     }, 
     
@@ -36,6 +36,33 @@ export const POKEMON: IContent[] = [
       imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/181.png',
       description: "The bright light on its tail can be seen far away. It has been treasured since ancient times as a beacon.",
       author: 'Jack Morgan',
-      type: 'Electric',
+      type: ['Electric'],
       tags: ["Electric", "#0169"]
-    }];
+    },
+    {
+      id: 5,
+      name: 'Stonejourner',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/874.png',
+      description: "The elemental composition of the rocks that form its body were found to match the bedrock of a land far away from this Pokémon’s habitat.",
+      author: 'Jack Morgan',
+      type: ['Rock'],
+      tags: ["Rock", "#0874"]
+    },
+    {
+      id: 6,
+      name: 'Houndstone',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/972.png',
+      description: "Houndstone spends most of its time sleeping in graveyards. Among all the dog Pokémon, this one is most loyal to its master.",
+      author: 'Jack Morgan',
+      tags: ["Ghost", "#0972"]
+    },
+    {
+      id: 7,
+      name: 'Necrozma',
+      imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/800.png',
+      description: "It looks somehow pained as it rages around in search of light, which serves as its energy. It’s apparently from another world.",
+      author: 'Jack Morgan',
+      type: ['Psychic'],
+      tags: ["Psychic", "#0972"]
+    }  
+  ];
